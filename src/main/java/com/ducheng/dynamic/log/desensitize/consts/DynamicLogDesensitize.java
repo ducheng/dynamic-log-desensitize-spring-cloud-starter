248c790c4008a40c5ccfd35d06ba379162f3e293
USER-20200810GJ\Administrator <13275768320@163.com>
2023-03-16 09:33:07 +0800

    修改配置文件

--- src/main/java/com/ducheng/dynamic/log/desensitize/consts/DynamicLogDesensitize.java
+++
@@ -1,8 +0,0 @@
-package com.ducheng.dynamic.log.desensitize.consts;
-
-public class DynamicLogDesensitize {
-
-    //yml加载的临时目录
-    public static final String YML_TEMPLATE_NAME = "TEMPLATE_NAME";
-
-}
